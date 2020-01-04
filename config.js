2fdc78c7b97c1e3ffc4d1046dfacd106d6df2b1c
Tri Nguyen <nvutri@gmail.com>
2020-01-04 16:21:38 +0700

    Make config.js localhost

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   db: process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://localhost:27017',
   server: {
-    port: process.env.PORT || 3010,
-    address: '0.0.0.0',
+    port: process.env.PORT || 3000,
+    address: 'localhost',
   },
   accessControl: {
     origin: '*',
